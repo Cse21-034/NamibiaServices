72ea45691c3024a108b0bf54f49bb88a437e3fa9
Cse21-034 <Cse21-034@thuto.bac.ac.bw>
2026-01-20 16:51:51 +0200

    fix list your bussiness border color

--- src/app/(client-components)/(Header)/MainNav1.tsx
+++ src/app/(client-components)/(Header)/MainNav1.tsx
@@ -167,7 +167,7 @@ const MainNav1: FC<MainNav1Props> = ({ className = "", isDashboard = false }) =>
         <div className="hidden md:flex flex-shrink-0 justify-end flex-1 lg:flex-none text-neutral-700 dark:text-neutral-100">
           <div className="flex items-center space-x-1">
             <SwitchDarkMode />
-            <ButtonSecondary href="/signup?tab=business-registration" className="border-burgundy-600 hover:bg-burgundy-50 dark:hover:bg-burgundy-900/20">
+            <ButtonSecondary href="/signup?tab=business-registration" className="border-2 border-burgundy-600 text-burgundy-600 hover:bg-burgundy-50 dark:hover:bg-burgundy-900/20 dark:border-burgundy-500 dark:text-burgundy-400">
               List Your Business
             </ButtonSecondary>
             <AuthButtons />
